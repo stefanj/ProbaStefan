6a6e8e760208282639c09c94f9ae0bbae114ef80
Mehmet <memetalce5@gmail.com>
2012-11-10 22:17:09 +0100

    Kako sakas

---
+++ src/Glavna.java
@@ -0,0 +1,15 @@
+
+public class Glavna {
+
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+RegistrationForm objekt=new RegistrationForm();
+objekt.setVisible(true);
+}
+
+}
+
+
